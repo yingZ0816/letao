e8ffa3b9f1f4d88235aced9e351cec9a7e4c23d5
zying <ying@qq.com>
2018-11-01 15:34:03 +0800

    用户管理页面分页功能完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,16 +1,36 @@
-// ajax请求用户数据
 $(function() {
-  $.ajax({
-    type: 'get',
-    url: '/user/queryUser',
-    data: {
-      page: 1,
-      pageSize: 5
-    },
-    dataType: 'json',
-    success: function(info) {
-      console.log(info);
-      $('tbody').html(template('tmp', info));
-    }
-  })
+  var currentPage = 1;
+  var pageSize = 5;
+  // 先渲染一次
+  render();
+
+  function render() {
+    // ajax请求用户数据
+    $.ajax({
+      type: 'get',
+      url: '/user/queryUser',
+      data: {
+        page: currentPage,
+        pageSize: pageSize
+      },
+      dataType: 'json',
+      success: function(info) {
+        // console.log(info);
+        $('tbody').html(template('tmp', info));
+        // 分页
+        var page = Math.ceil(info.total / info.size);
+        $('#paginator').bootstrapPaginator({
+          bootstrapMajorVersion: 3,
+          currentPage: currentPage,
+          totalPages: page,
+          onPageClicked: function(a, b, c, page) {
+            // console.log(page);
+            currentPage = page;
+            render();
+          }
+        })
+      }
+    })
+  }
+
 })
